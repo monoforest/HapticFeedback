0b6cf27a29eee4a2bb80d698037bd07c3159ffff
kwanyong park <monoforest@gmail.com>
2024-11-24 14:13:31 +0900

    Update HapticFeedback.mm

--- Assets/Plugins/CandyCoded.HapticFeedback/Plugins/iOS/HapticFeedback.mm
+++ Assets/Plugins/CandyCoded.HapticFeedback/Plugins/iOS/HapticFeedback.mm
@@ -1,44 +1,92 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+// 전역 변수로 피드백 제너레이터들을 유지
+static UISelectionFeedbackGenerator *selectionFeedbackGenerator = nil;
+static UINotificationFeedbackGenerator *notificationFeedbackGenerator = nil;
+
 extern "C"
 {
-
     void PerformHapticFeedback(const char* style) {
-
-        UIImpactFeedbackStyle feedbackStyle;
-
         const NSString* styleString = [NSString stringWithUTF8String: style];
-
-        if ([styleString isEqualToString:@"light"]) {
-
-            feedbackStyle = UIImpactFeedbackStyleLight;
-
-        } else if ([styleString isEqualToString:@"medium"]) {
-
-            feedbackStyle = UIImpactFeedbackStyleMedium;
-
-        } else if ([styleString isEqualToString:@"heavy"]) {
-
-            feedbackStyle = UIImpactFeedbackStyleHeavy;
-
-        } else {
-
-            NSException* exception = [NSException
-                                      exceptionWithName:NSInvalidArgumentException
-                                      reason:@"Invalid impact feedback style."
-                                      userInfo:nil];
-
-            [exception raise];
-
+        
+        // Impact Feedback (UIImpactFeedbackGenerator)
+        if ([styleString isEqualToString:@"light"] ||
+            [styleString isEqualToString:@"medium"] ||
+            [styleString isEqualToString:@"heavy"] ||
+            [styleString isEqualToString:@"soft"] ||
+            [styleString isEqualToString:@"rigid"]) {
+            
+            UIImpactFeedbackStyle feedbackStyle;
+            
+            if ([styleString isEqualToString:@"light"]) {
+                feedbackStyle = UIImpactFeedbackStyleLight;
+            } else if ([styleString isEqualToString:@"medium"]) {
+                feedbackStyle = UIImpactFeedbackStyleMedium;
+            } else if ([styleString isEqualToString:@"heavy"]) {
+                feedbackStyle = UIImpactFeedbackStyleHeavy;
+            } else if ([styleString isEqualToString:@"soft"]) {
+                if (@available(iOS 13.0, *)) {
+                    feedbackStyle = UIImpactFeedbackStyleSoft;
+                } else {
+                    feedbackStyle = UIImpactFeedbackStyleLight;
+                }
+            } else if ([styleString isEqualToString:@"rigid"]) {
+                if (@available(iOS 13.0, *)) {
+                    feedbackStyle = UIImpactFeedbackStyleRigid;
+                } else {
+                    feedbackStyle = UIImpactFeedbackStyleMedium;
+                }
+            }
+            
+            UIImpactFeedbackGenerator *feedbackGenerator = [[UIImpactFeedbackGenerator alloc] initWithStyle:feedbackStyle];
+            [feedbackGenerator prepare];
+            [feedbackGenerator impactOccurred];
+            
         }
-
-        UIImpactFeedbackGenerator *feedbackGenerator = [[UIImpactFeedbackGenerator alloc] initWithStyle:feedbackStyle];
-
-        [feedbackGenerator impactOccurred];
-
+        // Selection Feedback (UISelectionFeedbackGenerator)
+        else if ([styleString isEqualToString:@"selection"]) {
+            if (selectionFeedbackGenerator == nil) {
+                selectionFeedbackGenerator = [[UISelectionFeedbackGenerator alloc] init];
+            }
+            [selectionFeedbackGenerator prepare];
+            [selectionFeedbackGenerator selectionChanged];
+        }
+        // Notification Feedback (UINotificationFeedbackGenerator)
+        else if ([styleString isEqualToString:@"success"] ||
+                 [styleString isEqualToString:@"warning"] ||
+                 [styleString isEqualToString:@"error"]) {
+            
+            if (notificationFeedbackGenerator == nil) {
+                notificationFeedbackGenerator = [[UINotificationFeedbackGenerator alloc] init];
+            }
+            
+            [notificationFeedbackGenerator prepare];
+            
+            UINotificationFeedbackType feedbackType;
+            if ([styleString isEqualToString:@"success"]) {
+                feedbackType = UINotificationFeedbackTypeSuccess;
+            } else if ([styleString isEqualToString:@"warning"]) {
+                feedbackType = UINotificationFeedbackTypeWarning;
+            } else {
+                feedbackType = UINotificationFeedbackTypeError;
+            }
+            
+            [notificationFeedbackGenerator notificationOccurred:feedbackType];
+        }
+        else {
+            NSException* exception = [NSException
+                                    exceptionWithName:NSInvalidArgumentException
+                                    reason:@"Invalid feedback style."
+                                    userInfo:nil];
+            [exception raise];
+        }
     }
-
+    
+    // 메모리 관리를 위한 정리 함수
+    void CleanupHapticFeedback() {
+        selectionFeedbackGenerator = nil;
+        notificationFeedbackGenerator = nil;
+    }
 }
